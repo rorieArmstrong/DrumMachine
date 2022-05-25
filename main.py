fc7099c3f157e308abc4770479aff9dd73227080
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2022-05-25 15:50:35 +0100

    add play/pause

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ HEIGHT = 800
 black= (0,0,0)
 white = (255,255,255)
 grey = (128,128,128)
+dark_grey = (208,208,208)
 green = (0,255,0)
 red = (255, 0 ,0)
 gold = (212,175,55)
@@ -17,6 +18,7 @@ blue = (0, 255,255)
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
 pygame.display.set_caption('BEAT MAKER')
 label_font = pygame.font.Font('./fonts/Roboto-Bold.ttf', 32)
+sub_font = pygame.font.Font('./fonts/Roboto-Bold.ttf', 32)
 
 fps = 60
 timer = pygame.time.Clock()
@@ -24,7 +26,7 @@ drums = ['Hi Hat','Snare','Kick','Crash','Clap','Floor Tom']
 beats = 16
 boxes = []
 active = [[False for i in range(beats)] for j in drums]
-bpm = 108
+bpm = 168
 playing = True
 active_legnth = 0
 active_beat = 0
@@ -38,6 +40,7 @@ kick = mixer.Sound('./sounds/kick.WAV')
 snare = mixer.Sound('./sounds/snare.WAV')
 tom = mixer.Sound('./sounds/tom.WAV')
 sounds = [hi_hat,snare,kick,crash,clap,tom]
+mixer.set_num_channels(len(sounds)*3)
 
 def play_notes():
     for i in range(len(active)):
@@ -46,7 +49,6 @@ def play_notes():
 
 def draw_grid(active,beat):
     left_box = pygame.draw.rect(screen ,grey , [0,0,200,HEIGHT-200],5)
-    bottom_box = pygame.draw.rect(screen ,grey , [0,HEIGHT - 200,WIDTH,200],5)
     
     colours = [grey, white, grey]
 
@@ -78,6 +80,15 @@ while run:
     screen.fill(black)
     boxes = draw_grid(active,active_beat)
 
+    #Menu
+    bottom_box = pygame.draw.rect(screen ,grey , [0,HEIGHT - 200,WIDTH,200],5)
+    play_pause = pygame.draw.rect(screen ,grey , [50,HEIGHT - 150,200,100],0,5)
+    if playing:
+        play_text =label_font.render('Pause', True, white)
+    else:
+        play_text =label_font.render('Play', True, white)
+    screen.blit(play_text, (70, HEIGHT-115))
+
     if beat_changed:
         play_notes()
         beat_changed = False
@@ -90,6 +101,9 @@ while run:
                 if box[0].collidepoint(event.pos):
                     coords = box[1]
                     active[coords[1]][coords[0]] = not active[coords[1]][coords[0]]
+        if event.type == pygame.MOUSEBUTTONUP:
+            if play_pause.collidepoint(event.pos):
+                playing = not playing
     
     beat_legnth = 3600 // bpm
 
@@ -102,7 +116,7 @@ while run:
                 active_beat += 1
                 beat_changed = True
             else:
-                active_beat = 1
+                active_beat = 0
                 beat_changed = True
     
     pygame.display.flip()
